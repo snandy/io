8c4f10332ab114db8af4c36f0cb1050d514f4090
zhouyrt <zhouyrt@163.com>
2013-03-28 18:53:49 +0800

    ajax cros

--- ajax.js
+++ ajax.js
@@ -1,28 +1,18 @@
-/*******************************************************************************************************
- *
- * 1 使用新API解析JSON，对解析JSON出现可能出现的异常进行了处理
- * 2 超时处理做了修改，先给isTimeout赋值，再调用abort（不采用条件status==0指定超时）
- * 3 XHR创建方式修改（勿重复检测浏览器http://www.cnblogs.com/snandy/archive/2011/05/24/2055048.html）
- * 4 增加scope参数，success可指定上下文
- * 5 接口方式修改，增加get，post方法
- * 
- * 
- *******************************************************************************************************/
-
 /**
  * JavaScript Ajax tool
  * 
  * 1,执行基本ajax请求,返回XMLHttpRequest
  * Ajax.request(url, {
- *	 async   是否异步 true(默认)
- *	 method  请求方式 POST or GET(默认)
- *	 type	  数据格式 text(默认) or xml or json
- *	 encode  请求的编码 UTF-8(默认)
- *	 timeout 请求超时时间 0(默认)
- *	 data	  请求参数 (字符串或json)
- *	 scope   成功回调执行上下文
- *	 success 请求成功后响应函数 参数为text,json,xml数据
- *	 failure 请求失败后响应函数 参数为xmlHttp, msg, exp
+ *	 async   	是否异步 true(默认)
+ *	 method  	请求方式 POST or GET(默认)
+ *	 type	  	数据格式 text(默认) or xml or json
+ *	 encode  	请求的编码 UTF-8(默认)
+ *	 timeout 	请求超时时间 0(默认)
+ *	 credential 跨域请求时是否带证书(默认false，不带http认证信息如cookie) 
+ *	 data	  	请求参数 (字符串或json)
+ *	 scope   	成功回调执行上下文
+ *	 success 	请求成功后响应函数 参数为text,json,xml数据
+ *	 failure 	请求失败后响应函数 参数为xmlHttp, msg, exp
  * });
  * 
  * 2,执行ajax请求,返回纯文本
@@ -79,15 +69,16 @@ function request(url, opt) {
 		url = opt.url
 	}
 	var xhr, isTimeout, timer, opt = opt || {}
-	var async   = opt.async !== false,
-		method  = opt.method  || 'GET',
-		type	= opt.type	  || 'text',
-		encode  = opt.encode  || 'UTF-8',
-		timeout = opt.timeout || 0,
-		data	= opt.data,
-		scope   = opt.scope,
-		success = opt.success || noop,
-		failure = opt.failure || noop
+	var async      = opt.async !== false,
+		method     = opt.method  || 'GET',
+		type       = opt.type	  || 'text',
+		encode     = opt.encode  || 'UTF-8',
+		timeout    = opt.timeout || 0,
+		credential = opt.credential,
+		data	   = opt.data,
+		scope      = opt.scope,
+		success    = opt.success || noop,
+		failure    = opt.failure || noop
 		
 	method  = method.toUpperCase()
 	// 对象转换成字符串键值对
@@ -122,6 +113,9 @@ function request(url, opt) {
 		}
 	}
 	xhr.open(method,url,async)
+	if (credential) {
+		xhr.withCredentials = true
+	}
 	if (method == 'POST') {
 		xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;charset=' + encode)
 	}
@@ -164,8 +158,8 @@ function onStateChange(xhr, type, success, failure, scope) {
 }
 
 return (function() {
-	var Ajax = {request: request}, types = ['text','json','xml']
-	for (var i = 0, len = types.length; i<len; i++) {
+	var i, Ajax = {request: request}, types = ['text','json','xml']
+	for (i = 0, len = types.length; i<len; i++) {
 		Ajax[ types[i] ] = function(i) {
 			return function(url, opt) {
 				opt = opt || {}
@@ -178,3 +172,21 @@ return (function() {
 })()
 
 }(this);
+
+
+/*******************************************************************************************************
+ *
+ * 1 使用新API解析JSON，对解析JSON出现可能出现的异常进行了处理
+ * 2 超时处理做了修改，先给isTimeout赋值，再调用abort（不采用条件status==0指定超时）
+ * 3 XHR创建方式修改（勿重复检测浏览器http://www.cnblogs.com/snandy/archive/2011/05/24/2055048.html）
+ * 4 增加scope参数，success可指定上下文
+ * 5 接口方式修改，增加get，post方法
+ * 6 XHR2跨域实现带认证信息如cookie 后台需返回两个header （注意：目前最新Firefox/Chrome/Safari/Opera/IE10均支持）
+ * 	Access-Control-Allow-Origin: http://xxx.com
+ * 	Access-Control-Allow-Credentials: true
+ *  如果带认证信息，xhr设置 xhr.withCredentials = true;
+ * 	详见：
+ * 		https://developer.mozilla.org/en-US/docs/HTTP/Access_control_CORS
+ * 		http://enable-cors.org/client.html
+ * 
+ *******************************************************************************************************/
---
+++ test/Cors.java
@@ -0,0 +1,130 @@
+package app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Cors extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public Cors() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doDelete method of the servlet. <br>
+	 *
+	 * This method is called when a HTTP delete request is received.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doDelete(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("dd");
+		response.setContentType("text/plain");
+		response.setHeader("Access-Control-Allow-Origin", "http://base");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		PrintWriter out = response.getWriter();
+		out.println("{age:33}");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out
+				.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the POST method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPut method of the servlet. <br>
+	 *
+	 * This method is called when a HTTP put request is received.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Put your code here
+	}
+
+	/**
+	 * Returns information about the servlet, such as 
+	 * author, version, and copyright. 
+	 *
+	 * @return String information about this servlet
+	 */
+	public String getServletInfo() {
+		return "This is my default servlet created by Eclipse";
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
